d389ae024452efe652d7838af219154b938a6ad1
Nathan Liebrecht <nwliebrecht@gmail.com>
2019-03-15 16:05:35 -0600

    asdf

---
+++ api/app.py
@@ -0,0 +1,20 @@
+import music21
+
+# music21.environment.set('musicxmlPath', '/usr/bin/musescore')
+# river = music21.converter.parse('river.mxl')
+# riverChords = river.chordify()
+# riverChords.show('text')
+
+# for n in riverChords.getElementsByClass(music21.stream.Measure):
+#     print(n)
+# pass
+# riverChords.show()
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/api/')
+def hello_world():
+    return 'Hello, World!!!'
+
+app.run(host='0.0.0.0')
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,45 +3,46 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
 const config = {
-  entry: './index.ts',
-  mode: 'production',
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-    hot: true,
-    proxy: {
-      '/api': 'http://api:5000'
-    }
-  },
-  output: {
-    path: __dirname + '/dist',
-    filename: 'index.bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.json$/,
-        use: 'json-loader'
-      },
-      {
-        test: /\.ts$/,
-        use: 'ts-loader'
-      }
+    entry: './index.ts',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+        hot: true,
+        proxy: {
+            '/api': 'http://api:5000'
+        },
+        host: '0.0.0.0', // can be overwritten by process.env.HOST
+        port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    },
+    output: {
+        path: __dirname + '/dist',
+        filename: 'index.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.json$/,
+                use: 'json-loader'
+            },
+            {
+                test: /\.ts$/,
+                use: 'ts-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'opensheetmusicdisplay | webpack-usage-example',
+            // favicon: 'resources/favicon.ico' // not providing a favicon.ico can cause 404 warnings
+        }),
+        new CopyWebpackPlugin([
+            {
+                from: 'resources/SampleMusic.xml',
+                to: 'musicXmlSample.xml'
+            },
+        ]),
+        new webpack.HotModuleReplacementPlugin()
     ]
-  },
-  plugins: [
-  new HtmlWebpackPlugin({
-    title: 'opensheetmusicdisplay | webpack-usage-example',
-    // favicon: 'resources/favicon.ico' // not providing a favicon.ico can cause 404 warnings
-  }),
-  new CopyWebpackPlugin([
-  {
-    from: 'resources/SampleMusic.xml',
-    to: 'musicXmlSample.xml'
-  },
-  ]),
-  new webpack.HotModuleReplacementPlugin()
-  ]
 };
 
 module.exports = config;
