df3d81795ad9ce7c06199fcda8b912b6fbd7fb81
Nathan Liebrecht <nwliebrecht@gmail.com>
2019-03-15 14:50:39 -0600

    asdf

--- api/main.py
+++ api/main.py
@@ -3,4 +3,9 @@ import music21
 music21.environment.set('musicxmlPath', '/usr/bin/musescore')
 river = music21.converter.parse('river.mxl')
 riverChords = river.chordify()
-riverChords.show()
+riverChords.show('text')
+
+# for n in riverChords.getElementsByClass(music21.stream.Measure):
+#     print(n)
+# pass
+# riverChords.show()
