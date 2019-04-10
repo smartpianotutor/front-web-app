7dda979dfc9c11817eec78bb7e25e09655d9b94b
Abid Rahman <arahman1@ualberta.ca>
2019-04-10 14:48:25 -0600

    Remove empty test suites

--- frontend/src/requirements/ability.ts
+++
@@ -1,2 +0,0 @@
-// -3.2.4.3.1 The system shall display the user's ability vector magnitude (and change in magnitude) for notes in the most recent practice snippet upon completion of said practice snippet.
-// -3.2.5.3.1 The front-end system shall send the performance information to the back-end system for score generation upon completion of a snippet. 
